cff93c3620e5513a0303ed8b24a2093113ae5763
Phucdz11 <136161215+Phucdz11@users.noreply.github.com>
2025-01-20 22:11:04 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,17 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({ 
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'info'
+    host: 'sql12.freesqldatabase.com',
+    user: 'sql12758666',          // Username
+    password: 'PVrSmiIpYR', // Mật khẩu thực tế (bạn kiểm tra email để lấy)
+    database: 'sql12758666'       // Tên database
 });
 
 db.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        console.error('Database connection failed: ', err.stack);
+        return;
+    }
     console.log('Connected to database');
 });
 
